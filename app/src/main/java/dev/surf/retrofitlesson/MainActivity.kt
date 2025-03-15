42bed9c3c3219f2e0efdf1c2b020b16edc840530
selin <selin@surf.dev>
2025-03-15 15:54:46 +0300

    Первоначальная настройка Hilt
    - Создаем Application класс
    - Вешаем на него аннотацию HiltAndroidApp
    - Не забываем в манифесте указать приложению на наш App класс
    - Чтобы Hilt заработал вешаем на MainActivity класс аннотацию AndroidEntryPoint
    
    Все, hilt готов к использованию

--- app/src/main/java/dev/surf/retrofitlesson/MainActivity.kt
+++ app/src/main/java/dev/surf/retrofitlesson/MainActivity.kt
@@ -10,9 +10,11 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
 import dev.surf.retrofitlesson.presentation.navigation.AppNavigationGraph
 import dev.surf.retrofitlesson.presentation.ui.theme.SurfRetrofitLessonTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
