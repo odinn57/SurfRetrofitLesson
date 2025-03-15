717cbffac31625c015663ced3cabe25ede329895
selin <selin@surf.dev>
2025-03-15 17:57:58 +0300

    Логика экрана отображения списка постов
    - в MainViewModel инжектим нужные для работы экрана useCase()
    - для удобства реагирования на ответы с сервера создадим расширение, для выполнения определенных действия в зависимости от OperationResult
    - реализуем оставшиеся методы экрана, начнем с главного, с getAllPosts для получения полного списка постов

--- app/src/main/java/dev/surf/retrofitlesson/presentation/screens/main/MainViewModel.kt
+++ app/src/main/java/dev/surf/retrofitlesson/presentation/screens/main/MainViewModel.kt
@@ -1,15 +1,27 @@
 package dev.surf.retrofitlesson.presentation.screens.main
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.surf.retrofitlesson.domain.models.PostModel
+import dev.surf.retrofitlesson.domain.usecase.post.CreatePostUseCase
+import dev.surf.retrofitlesson.domain.usecase.post.DeletePostUseCase
+import dev.surf.retrofitlesson.domain.usecase.post.GetAllPostsUseCase
+import dev.surf.retrofitlesson.domain.usecase.post.UpdatePostUseCase
+import dev.surf.retrofitlesson.presentation.screens.utils.handle
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor() : ViewModel() {
+class MainViewModel @Inject constructor(
+    private val getAllPostsUseCase: GetAllPostsUseCase,
+    private val createPostUseCase: CreatePostUseCase,
+    private val deletePostUseCase: DeletePostUseCase,
+    private val updatePostUseCase: UpdatePostUseCase
+) : ViewModel() {
     private val _uiState = MutableStateFlow(MainScreenState())
     val uiState = _uiState.asStateFlow()
 
@@ -22,20 +34,52 @@ class MainViewModel @Inject constructor() : ViewModel() {
     }
 
     fun createNew() {
-        // TODO("Отправить запрос на создание поста")
+        viewModelScope.launch {
+            createPostUseCase(
+                userId = 101,
+                title = "New Title",
+                body = "New Body"
+            )
+        }
     }
 
     fun onDeletePost(id: Int) {
-        // TODO("Отправить запрос на удаление поста")
+        viewModelScope.launch {
+            deletePostUseCase(id)
+        }
     }
 
     fun onEditPost(post: PostModel) {
-        // TODO("Отправить запрос на редактирование поста")
+        viewModelScope.launch {
+            updatePostUseCase(
+                id = post.id,
+                userId = post.userId,
+                title = "Updated Title",
+                body = "Updated Body",
+            )
+        }
     }
 
     private fun getAllPosts() {
-        // TODO("Отправить запрос на получение списка постов")
-        onError("Not implement yet!")
+        _uiState.update { state ->
+            state.copy(
+                isLoading = true
+            )
+        }
+        viewModelScope.launch {
+            getAllPostsUseCase().handle(
+                onSuccess = { postList ->
+                    _uiState.update { state ->
+                        state.copy(
+                            isLoading = false,
+                            postList = postList,
+                            errorMessage = null
+                        )
+                    }
+                },
+                onError = ::onError
+            )
+        }
     }
 
     private fun onError(message: String) {
