ba2ab18efb0c1572d9c11eb95e95ba7a7ff6f008
selin <selin@surf.dev>
2025-03-15 18:16:56 +0300

    Логика экрана деталки поста
    - в PostDetailsViewModel инжектим нужные для работы экрана useCase()
    - реализуем оставшиеся методы экрана, начнем с getPost для получения данных о посте

--- app/src/main/java/dev/surf/retrofitlesson/presentation/screens/postDetails/PostDetailsViewModel.kt
+++ app/src/main/java/dev/surf/retrofitlesson/presentation/screens/postDetails/PostDetailsViewModel.kt
@@ -2,16 +2,24 @@ package dev.surf.retrofitlesson.presentation.screens.postDetails
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.surf.retrofitlesson.domain.models.PostModel
+import dev.surf.retrofitlesson.domain.usecase.post.GetPostByIdUseCase
+import dev.surf.retrofitlesson.domain.usecase.post.UpdatePostUseCase
 import dev.surf.retrofitlesson.presentation.navigation.Route
+import dev.surf.retrofitlesson.presentation.screens.utils.handle
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class PostDetailsViewModel @Inject constructor(
+    private val getPostByIdUseCase: GetPostByIdUseCase,
+    private val updatePostUseCase: UpdatePostUseCase,
     savedStateHandle: SavedStateHandle
 ) : ViewModel() {
     private val _uiState: MutableStateFlow<PostDetailsScreenState>
@@ -32,9 +40,42 @@ class PostDetailsViewModel @Inject constructor(
         getPost(_uiState.value.postId)
     }
 
+    fun updatePost(postModel: PostModel) {
+        viewModelScope.launch {
+            updatePostUseCase(
+                id = postModel.id,
+                userId = postModel.userId,
+                title = "New Post Title",
+                body = "New Post Body"
+            )
+        }
+    }
+
     private fun getPost(id: Int) {
-        // TODO("Отправить запрос на получение информации о посте")
-        onError("Not implement yet!")
+        _uiState.update { state ->
+            state.copy(
+                isLoading = true
+            )
+        }
+        if (id != -1) {
+            viewModelScope.launch {
+                getPostByIdUseCase(id).handle(
+                    onSuccess = { postDetails ->
+                        _uiState.update { state ->
+                            state.copy(
+                                isLoading = false,
+                                postDetails = postDetails,
+                                errorMessage = null
+                            )
+                        }
+                    },
+                    onError = ::onError,
+                )
+
+            }
+        } else {
+            onError("Post id = $id is not correct!")
+        }
     }
 
 
