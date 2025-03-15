4b7dab6b1839c0e094a8a9ae51be74d7f18885da
selin <selin@surf.dev>
2025-03-15 15:09:25 +0300

    Добавление зависимостей
    С помощью toml были описаны все необходимые для работы зависимости. Далее они были подключены в build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,8 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+
+    // Настройка плагинов для Hilt
+    alias(libs.plugins.hiltAndroid) apply false
+    alias(libs.plugins.kotlinKapt) apply false
 }
