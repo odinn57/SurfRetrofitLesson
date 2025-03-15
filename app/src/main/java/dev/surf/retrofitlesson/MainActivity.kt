38474a5809fffbf7fea5396e71c7bc2dadaa8e00
selin <selin@surf.dev>
2025-03-15 15:35:42 +0300

    Перенос темы в presentation модуль

--- app/src/main/java/dev/surf/retrofitlesson/MainActivity.kt
+++ app/src/main/java/dev/surf/retrofitlesson/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import dev.surf.retrofitlesson.ui.theme.SurfRetrofitLessonTheme
+import dev.surf.retrofitlesson.presentation.ui.theme.SurfRetrofitLessonTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/dev/surf/retrofitlesson/presentation/ui/theme/Theme.kt
+++ app/src/main/java/dev/surf/retrofitlesson/presentation/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package dev.surf.retrofitlesson.ui.theme
+package dev.surf.retrofitlesson.presentation.ui.theme
 
 import android.app.Activity
 import android.os.Build
